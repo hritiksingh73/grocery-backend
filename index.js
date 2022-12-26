df9955361beaf772d5bea516c4cd6a26eedd5aec
hritik.singh <hritik.singh@indianic.com>
2022-12-26 11:18:21 +0530

    feat: 1st version

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+
+import { popularProducts } from './src/data/index.js';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send({
+    github: 'https://github.com/hrithik73',
+  });
+});
+
+app.get('/products/popular', (req, res) => {
+  res.send(popularProducts);
+});
+
+app.get('/products/:prodID', (req, res) => {
+  const { prodID } = req.params;
+  const product = popularProducts.filter((item) => item.id === prodID);
+  res.send(product);
+});
+
+app.listen(port, () =>
+  console.log('🚀 Server ready at: http://localhost:3000')
+);
---
+++ src/data/index.js
@@ -0,0 +1,99 @@
+export const popularProducts = [
+  {
+    id: 'a11',
+    title: 'Banana',
+    oldPrice: 1,
+    price: 0.27,
+    imageUrl:
+      'https://img.freepik.com/premium-photo/banana-bunch-isolated_88281-1027.jpg?w=2000',
+  },
+
+  {
+    id: 'b22',
+    title: 'Broccoli',
+    oldPrice: 2,
+    price: 0.27,
+    imageUrl:
+      'https://rukminim1.flixcart.com/image/312/312/ktlu9ow0/plant-seed/b/i/n/1-hybrid-brocoli-hardik-enterprises-original-imag6wu6rzzxzrhz.jpeg?q=70',
+  },
+
+  {
+    id: 'c33',
+    title: 'Carrots',
+    oldPrice: 1.2,
+    price: 0.27,
+    imageUrl:
+      'https://www.collinsdictionary.com/images/full/carrot_125262134.jpg',
+  },
+  {
+    id: 'd44',
+    title: 'Cabbage',
+    oldPrice: 1.2,
+    price: 0.27,
+    imageUrl:
+      'https://blog-images.pharmeasy.in/2021/02/25184143/shutterstock_1282068550-1-18.jpg',
+  },
+  {
+    id: 'e55',
+    title: 'Garlic',
+    oldPrice: 2.3,
+    price: 0.27,
+    imageUrl:
+      'https://5.imimg.com/data5/EQ/CG/MY-41917443/fresh-garlic-500x500.jpg',
+  },
+  {
+    id: 'f66',
+    title: 'Ginger',
+    oldPrice: 1.2,
+    price: 0.27,
+    imageUrl:
+      'https://d2cbg94ubxgsnp.cloudfront.net/Pictures/2000xAny/6/3/7/108637_ginger_shutterstock_291717662.jpg',
+  },
+  {
+    id: 'g77',
+    title: 'Onion',
+    oldPrice: 1,
+    price: 0.27,
+    imageUrl:
+      'https://www.macmillandictionary.com/external/slideshow/full/135967_full.jpg',
+  },
+  {
+    id: 'h88',
+    title: 'Apple',
+    price: 0.27,
+    imageUrl:
+      'https://5.imimg.com/data5/GLADMIN/Default/2022/7/BE/AS/OV/92368/apple-500x500.jpg',
+  },
+  {
+    id: 'i99',
+    title: 'Grapes',
+    price: 0.27,
+    oldPrice: 1,
+    imageUrl:
+      'https://i0.wp.com/alangulam.online/wp-content/uploads/2021/12/green-grapes.jpg?fit=1080%2C1080&ssl=1',
+  },
+  {
+    id: 'j02',
+    title: 'Pomegranate',
+    oldPrice: 1,
+    price: 0.27,
+    imageUrl: 'https://static.toiimg.com/photo/msid-69940581/69940581.jpg',
+  },
+
+  {
+    id: 'k32',
+    title: 'Tomato',
+    oldPrice: 1,
+    price: 0.27,
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYVU-GFStOSmXTIp1pnOnNvR2ykxp-DY1a4w&usqp=CAU',
+  },
+  {
+    id: 'l31',
+    title: 'Potato',
+    oldPrice: 1,
+    price: 0.27,
+    imageUrl:
+      'https://www.harishfoodzone.com/jb-content/uploads/2020/11/potato.jpg',
+  },
+];
